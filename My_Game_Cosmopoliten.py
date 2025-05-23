74dde42a317e73ceee07c8c82dfda5215b3cac03
kuceravink <kuceravink@gmail.com>
2025-05-24 00:27:34 +0500

    beta

--- My_Game_Cosmopoliten.py
+++ My_Game_Cosmopoliten.py
@@ -3,7 +3,8 @@ import random
 from os import path
 from PIL import Image, ImageSequence
 
-
+#480
+#600
 WIDTH = 480
 HEIGHT = 600
 FPS = 144
@@ -15,20 +16,41 @@ BLACK = (0, 0, 0)
 pygame.init()
 pygame.mixer.init() 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('My_first_Gaame')
+pygame.display.set_caption('COSMOPOLITEN')
 clock = pygame.time.Clock()
 
+
+
+img_dir = path.join(path.dirname(__file__), 'img')
 snd_dir = path.join(path.dirname(__file__), 'music')
 shoot_m = pygame.mixer.Sound(path.join(snd_dir, 'Laser.wav'))
 expl = pygame.mixer.Sound(path.join(snd_dir, 'explosion01.wav'))
 expl.set_volume(0.09)
 pygame.mixer.music.load(path.join(snd_dir, 'hull_et_belle.ogg'))
 pygame.mixer.music.set_volume(0.4)
+heart = pygame.image.load(path.join(img_dir, 'heart pixel art 32x32.png')).convert()
+heart.set_colorkey(BLACK)
 background = pygame.image.load('C:\\Users\\kucer\\OneDrive\\Desktop\\My_Game\\bg_space_seamless.png').convert()
 background = pygame.transform.scale(background, (480, 600))
 background_rect = background.get_rect()
 
-
+blast = {}
+blast['bg'] = []
+blast['sm'] = []
+blast['player'] = []
+for i in range(9):
+    name_img = 'regularExplosion0{}.png'.format(i)
+    img = pygame.image.load(path.join(img_dir, name_img)).convert()
+    img.set_colorkey(BLACK)
+    img_bg = pygame.transform.scale(img, (75, 75))
+    blast['bg'].append(img_bg)
+    img_sm = pygame.transform.scale(img, (32, 32))
+    blast['sm'].append(img_sm)
+    name_img = 'sonicExplosion0{}.png'.format(i)
+    img = pygame.image.load(path.join(img_dir, name_img)).convert()
+    img.set_colorkey(BLACK)
+    blast['player'].append(img)
+    
 def load_gif(path):
     gif = Image.open(path)  # Открываем GIF с помощью Pillow
     frames = []  # Сюда будем складывать кадры в формате Pygame
@@ -53,6 +75,24 @@ def draw_text(surf, text, size, x, y):
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
 
+def draw_health(surf, x, y, pct):
+    if pct < 0:
+        pct = 0
+    _LENGTH = 100
+    _HEIGHT = 10
+    fill = (pct / 100) * _LENGTH
+    outline_rect = pygame.Rect(x, y, _LENGTH, _HEIGHT)
+    fill_rect = pygame.Rect(x, y, fill, _HEIGHT)
+    pygame.draw.rect(surf, WHITE, fill_rect)
+    pygame.draw.rect(surf, BLUE, outline_rect, 2)
+
+def draw_hearts(surf, x, y, heart, img):
+    for i in range(heart):
+        img_rect = img.get_rect()
+        img_rect.x = x + 30 * i
+        img_rect.y = y
+        surf.blit(img, img_rect)
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -73,9 +113,22 @@ class Player(pygame.sprite.Sprite):
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
+        self.health = 100
+        self.shoot_pause = 250
+        self.last_shot = pygame.time.get_ticks() 
+        self.hearts = 3
+        self.hides = False
+        self.hide_time = pygame.time.get_ticks()
+
 
 
     def update(self):
+        if self.hides and pygame.time.get_ticks() - self.hide_time > 1000:
+            self.hides = False
+            self.rect.centerx = WIDTH / 2
+            self.rect.bottom = HEIGHT - 10
+
+
         # Движение
         self.speedx = 0
         self.speedy = 0
@@ -90,6 +143,8 @@ class Player(pygame.sprite.Sprite):
             self.speedy = -3
         if keystate[pygame.K_DOWN]:
             self.speedy = 3
+        if keystate[pygame.K_SPACE]:
+            self.shoot()
         self.rect.y += self.speedy
         # Границы экрана
 
@@ -104,17 +159,28 @@ class Player(pygame.sprite.Sprite):
 
         # Анимация
         self.frame_count += 1
-        if self.frame_count >= self.animation_speed * 60:  # 60 FPS
+        if self.frame_count >= self.animation_speed * 144:  # 144 FPS
             self.frame_count = 0
             self.current_frame = (self.current_frame + 1) % len(self.frames)
             self.image = self.frames[self.current_frame]
             self.image.set_colorkey(WHITE)  # Обновляем colorkey для нового кадра
 
+        
+
     def shoot(self):
-        bullet = Bullet(self.rect.centerx, self.rect.top)
-        all_sprites.add(bullet)
-        bullets.add(bullet)
-        shoot_m.play()
+        now = pygame.time.get_ticks()
+        if now - self.last_shot > self.shoot_pause:
+            self.last_shot = now
+            bullet = Bullet(self.rect.centerx, self.rect.top)
+            all_sprites.add(bullet)
+            bullets.add(bullet)
+            shoot_m.play()
+
+    def hide(self):
+    # временно скрыть игрока
+        self.hides = True
+        self.hide_time = pygame.time.get_ticks()
+        self.rect.center = (WIDTH / 2, HEIGHT + 200)
 
 
 class Mobs(pygame.sprite.Sprite):
@@ -173,7 +239,6 @@ class Mobs(pygame.sprite.Sprite):
             self.image = self.image_orig.copy()
             self.rect = self.image.get_rect()
             self.radius = int(self.rect.width * .85 / 2)
-            
             self.rect.x = random.randrange(WIDTH - self.rect.width)
             self.rect.y = random.randrange(-100, -40)
             self.speedy = random.randrange(2, 5)
@@ -193,15 +258,42 @@ class Bullet(pygame.sprite.Sprite):
         if self.rect.bottom < 0:
             self.kill()
 
+class Blast(pygame.sprite.Sprite):
+    def __init__(self, center, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = size
+        self.image = blast[self.size][0]
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.frame = 0
+        self.last_update = pygame.time.get_ticks()
+        self.frame_upd = 50
+    def update(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > self.frame_upd:
+            self.last_update = now
+            self.frame += 1
+            if self.frame == len(blast[self.size]):
+                self.kill()
+            else:
+                center = self.rect.center
+                self.image = blast[self.size][self.frame]
+                self.rect = self.image.get_rect()
+                self.rect.center = center
+
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
 player = Player()
 bullets = pygame.sprite.Group()
 all_sprites.add(player)
-for i in range(5):
+
+def mobspawn():
     m = Mobs()
     all_sprites.add(m)
     mobs.add(m)
+
+for i in range(5):
+    mobspawn()
 
 score = 0
 pygame.mixer.music.play(loops=-1)
@@ -214,32 +306,56 @@ while running:
         # делаем возможноть закрыть окно крестиком
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player.shoot()
+
 
     #обновление
     all_sprites.update()
+
     hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
     for hit in hits:
         m = Mobs()
         expl.play()
         score += 50 - hit.radius
         all_sprites.add(m)
-        mobs.add(m)
-
+        mobs.add(m) 
+        blst = Blast(hit.rect.center, 'bg')
+        all_sprites.add(blst)
+        mx_score = 0
+        next_threshold = mx_score + 500
+        if score >= next_threshold:
+            mobspawn()  
+            mx_score = next_threshold
+        
 
     #проверка не ударил ли моб игрока
-    hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
-    if hits:
+    hits = pygame.sprite.spritecollide(player, mobs, True, pygame.sprite.collide_circle)
+    for hit in hits:
+        player.health -= hit.radius * 2
+        blst = Blast(hit.rect.center, 'sm')
+        all_sprites.add(blst)
+        mobspawn()
+        if player.health < 0:
+            all_sprites.add(Blast(player.rect.center, 'player'))
+            player.hide()
+            player.hearts -= 1
+            player.health = 100
+
+    if player.hearts == 0:
         running = False
+
+    if not player.alive() and not Blast(player.rect.center, 'player').alive():
+        running = False
+
+
 
     # Рендеринг
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     #отрисовываем все спрайты
     all_sprites.draw(screen)
-    draw_text(screen, str(score), 25 , WIDTH / 2, 10)
+    draw_text(screen, str(score), 18, WIDTH / 2, 10)
+    draw_health(screen, 5, 5, player.health)
+    draw_hearts(screen, WIDTH - 100, 5, player.hearts, heart)
     # поворачиваем экран к пользователю(мы отрисовали только заднюю сторону)
     pygame.display.flip()
 
